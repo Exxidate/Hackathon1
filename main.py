3b39e2b47c7693780a7bb3fb592cd26f5035a37c
mairinakmaria-svg <mairinakmaria@knu.ua>
2026-02-17 12:14:02 +0200

    Fix merge markers in main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import utils
 
 print(utils.factorial(10))
-<<<<<<< HEAD
 print(utils.is_prime(7))
 print(utils.fib(8))
 print(utils.pow_five(625))
-=======
 print(utils.gcd(24, 18))
->>>>>>> ce1d43d (Add call to gcd function in main)
