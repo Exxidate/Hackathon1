f2aea7a37ab70b93ae5029fa193b7abc1e5287c8
mairinakmaria-svg <mairinakmaria@knu.ua>
2026-02-17 11:35:32 +0200

    Add gcd function and fix is_prime

--- utils.py
+++ utils.py
@@ -1,15 +1,22 @@
 def factorial(n):
     prod = 1
-
-    for i in range(2, n+1):
-        prod*=i
-    
+    for i in range(2, n + 1):
+        prod *= i
     return prod
 
+
 def is_prime(n):
-    
-    for i in range(2, n**(1/2)+1):
-        if n%i == 0:
+    if n <= 1:
+        return False
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
             return False
-    
+
     return True
+
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
